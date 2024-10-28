061865c01f8c19862a896d966fc68f8881967941
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-10-28 22:41:07 +0000

    Fix error in playRound and added playGame

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function getComputerChoice()
 function getHumanChoice(){
     let valid = ["rock","paper","scissors"]
     let invalid = true
-    let choice =""
+    let choice = ""
     while(invalid){
-        let choice = prompt("rock, paper, or scissors?")
+        choice = prompt("rock, paper, or scissors?")
         if(valid.includes(choice.toLowerCase())){
             invalid=false
             
@@ -32,36 +32,36 @@ function getHumanChoice(){
 
 function playRound(humanChoice, computerChoice){
   
-    if(humanChoice = "rock"){
-        if(computerChoice="paper"){
+    if(humanChoice == "rock"){
+        if(computerChoice=="paper"){
             computerScore++
             console.log("YOU LOSE, paper beats rock")
         }
-        else if(computerChoice="scissors"){
+        else if(computerChoice=="scissors"){
             humanScore++
             console.log("YOU WIN, rock beats scissors")
         }else{
             console.log("DRAW, both picked rock")
         }
     }
-    else if(humanChoice = "paper"){
-        if(computerChoice="scissors"){
+    else if(humanChoice == "paper"){
+        if(computerChoice=="scissors"){
             computerScore++
             console.log("YOU LOSE, scissors beats paper")
         }
-        else if(computerChoice="rock"){
+        else if(computerChoice=="rock"){
             humanScore++
             console.log("YOU WIN, paper beats rock")
         }else{
             console.log("DRAW, both picked paper")
         }
     }
-    else if(humanChoice = "scissors"){
-        if(computerChoice="rock"){
+    else if(humanChoice == "scissors"){
+        if(computerChoice=="rock"){
             computerScore++
             console.log("YOU LOSE, rock beats scissors")
         }
-        else if(computerChoice="paper"){
+        else if(computerChoice=="paper"){
             humanScore++
             console.log("YOU WIN, scissors beats paper")
         }else{
@@ -71,14 +71,32 @@ function playRound(humanChoice, computerChoice){
 }
 
 
+function playGame(){
+    humanScore = 0
+    computerScore = 0
+    let humanChoice 
+    let computerChoice 
+
+    for(let i = 0; i<5;i++){
+        humanChoice = getHumanChoice()
+        computerChoice = getComputerChoice()
+        console.log("Human: "+humanChoice)
+        console.log("computer: "+computerChoice)
+        playRound(humanChoice,computerChoice)
+        
+    }
+
+    if(humanScore>computerScore){
+        console.log("Well done you win the game " +humanScore+" to "+computerScore)
+    }else if(humanScore<computerScore){
+        console.log("Unlucky, you lost the game "+humanScore+" to "+computerScore)
+    }else{
+        console.log("wow the game was a draw "+humanScore+" - "+computerScore)
+    }
+}
+let humanScore
+let computerScore 
+playGame()
 
 
-let humanScore = 0
-let computerScore =0
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
-    
-playRound(humanChoice,computerChoice)
 
-console.log(getComputerChoice());
-console.log(getHumanChoice())
