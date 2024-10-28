cc891a613c7c3127f54e73cd96a7534d890c87b4
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-10-28 22:15:39 +0000

    Added a play round function

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function getComputerChoice()
 {
     let randNum = Math.random();
     if(randNum<=1/3){
-        return "Rock"
+        return "rock"
     }
     else if(randNum<=2/3){
-        return "Paper"
+        return "paper"
     }
     else{
-        return "Scissors"
+        return "scissors"
     }
 }
 
@@ -27,13 +27,58 @@ function getHumanChoice(){
 
         }
     }
-    return choice
+    return choice.toLowerCase()
+}
+
+function playRound(humanChoice, computerChoice){
+  
+    if(humanChoice = "rock"){
+        if(computerChoice="paper"){
+            computerScore++
+            console.log("YOU LOSE, paper beats rock")
+        }
+        else if(computerChoice="scissors"){
+            humanScore++
+            console.log("YOU WIN, rock beats scissors")
+        }else{
+            console.log("DRAW, both picked rock")
+        }
+    }
+    else if(humanChoice = "paper"){
+        if(computerChoice="scissors"){
+            computerScore++
+            console.log("YOU LOSE, scissors beats paper")
+        }
+        else if(computerChoice="rock"){
+            humanScore++
+            console.log("YOU WIN, paper beats rock")
+        }else{
+            console.log("DRAW, both picked paper")
+        }
+    }
+    else if(humanChoice = "scissors"){
+        if(computerChoice="rock"){
+            computerScore++
+            console.log("YOU LOSE, rock beats scissors")
+        }
+        else if(computerChoice="paper"){
+            humanScore++
+            console.log("YOU WIN, scissors beats paper")
+        }else{
+            console.log("DRAW, both picked scissors")
+        }
+    }
 }
 
 
 
-    
 
+let humanScore = 0
+let computerScore =0
+let humanChoice = getHumanChoice()
+let computerChoice = getComputerChoice()
+    
+playRound(humanChoice,computerChoice)
 
 console.log(getComputerChoice());
 console.log(getHumanChoice())
